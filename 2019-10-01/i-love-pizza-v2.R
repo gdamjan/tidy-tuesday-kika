735e6381fdaf9cb5ec17e6b8892c0e7fa0a339e7
teofiln <teofiln@gmail.com>
2019-10-02 01:22:07 -0500

    reorganizing code and minor edits

--- 2019-10-01/i-love-pizza-v2.R
+++ 2019-10-01/i-love-pizza-v2.R
@@ -43,35 +43,78 @@ rate_diff_top5 <- rate_diff %>%
   top_n(n = 5, wt = abs(difference)) %>% 
   ungroup
 
+# more modifications to this data frame
+# to make coordinates for segments 
+# for manual labels
+
+rdt5 <- 
+  rate_diff_top5 %>% 
+  mutate(name_city= paste(name, "\n(", city, ")", sep="")) %>% 
+  select(name, name_city, contains("dave"), contains("community"), pos_neg, difference) %>% 
+  arrange(pos_neg, desc(difference)) %>% 
+  mutate(xend=c(rep(10.1,5), rep(2.4, 5))) %>% 
+  mutate(yend=c(5,2,3,4,6, 7,6,10,9,8)) %>% 
+  rename(x=review_stats_community_average_score,
+         y=review_stats_dave_average_score)
+
+
 # should have everything to build the plot
 
-ggplot(data = pzbs) +
-  geom_abline(slope = 1, intercept = 0, linetype=2) +
+## geoms
+PP <-
+  ggplot(data = pzbs) +
+  geom_abline(slope = 1,
+              intercept = 0,
+              linetype = 2) +
   aes(x = review_stats_community_average_score) +
   aes(y = review_stats_dave_average_score) +
   geom_point(
     data = rate_diff,
     inherit.aes = TRUE,
-    color="white",
+    color = "white",
     aes(size = abs(difference), fill = difference),
     pch = 21
-  ) + 
-  # geom_smooth(method=lm) +
-  scale_size(guide = "none") +
-  theme_minimal() +
-  theme(legend.position = 'bottom') +
-  theme(panel.background = element_rect(fill="grey", color="grey")) +
-  theme(plot.background = element_rect(fill="grey", color="grey")) +
-  theme(axis.title=element_text(hjust=1, size=12)) +
-  theme(axis.text=element_text(hjust=1, size=10)) +
+  ) +
+  geom_segment(
+    data = rdt5,
+    size = .5,
+    linetype = 3,
+    aes(
+      x = x,
+      y = y,
+      xend = xend,
+      yend = yend
+    )
+  ) +
+  geom_label(
+    data = rdt5,
+    aes(
+      x = xend,
+      y = yend,
+      fill = difference,
+      label = str_wrap(name, 22)
+    ),
+    size = 3.5,
+    hjust = c(rep(0, 5), rep(1, 5))
+  )
+
+## scales
+PP <- PP +
   scale_fill_gradient2(
     name = "",
     low = vv[1],
     mid = vv[2],
     high = vv[3],
-    limits=c(-5, 7.5),
-    breaks= c(-5, -2.5, 0, 2.5, 5, 7.5),
-    labels= c("Dave's favorites\nrated low by the community", "", "", "", "", "Community favorites\nrated low by Dave"),
+    limits = c(-5, 7.5),
+    breaks = c(-5,-2.5, 0, 2.5, 5, 7.5),
+    labels = c(
+      "Dave's favorites\nrated low by the community",
+      "",
+      "",
+      "",
+      "",
+      "Community favorites\nrated low by Dave"
+    ),
     guide = guide_colourbar(
       title.vjust = 1,
       frame.colour = 'white',
@@ -85,47 +128,30 @@ ggplot(data = pzbs) +
       draw.llim = TRUE
     )
   ) +
-  labs(x="Community rating (average)") +
-  labs(y="Dave's rating") +
-  labs(title="Trust Dave?") +
-  labs(subtitle="Or follow the crowd?") +
-  labs(caption="TidyTuesdayAtKIKA using pizza_barstool data") +
-  coord_cartesian() +
-  xlim(0, 12) + ylim(0,12) +
-  scale_y_continuous(breaks=seq(0,10,2)) -> PP
+  scale_y_continuous(breaks = seq(0, 10, 2), limits = c(0, 12)) +
+  scale_x_continuous(breaks = seq(0, 10, 2), limits = c(0, 12)) +
+  scale_size_continuous(range = c(0.5, 8), guide = "none")
 
-rdt5 <- 
-  rate_diff_top5 %>% 
-  mutate(name_city= paste(name, "\n(", city, ")", sep="")) %>% 
-  select(name, name_city, contains("dave"), contains("community"), pos_neg, difference) %>% 
-  arrange(pos_neg, desc(difference)) %>% 
-  mutate(xend=c(rep(10.1,5), rep(2.4, 5))) %>% 
-  mutate(yend=c(5,2,3,4,6, 7,6,10,9,8)) %>% 
-  rename(x=review_stats_community_average_score,
-         y=review_stats_dave_average_score)
+## labs
+PP <- PP +
+  labs(x = "Community rating (average)") +
+  labs(y = "Dave's rating") +
+  labs(title = "Trust Dave?") +
+  labs(subtitle = "Or follow the crowd?") +
+  labs(caption = "TidyTuesdayAtKIKA using pizza_barstool data")
 
+## theme
+PP <- PP +
+  theme_minimal() +
+  theme(legend.position = 'bottom') +
+  theme(panel.background = element_rect(fill = "grey60", color = "white")) +
+  theme(plot.background = element_rect(fill = "grey60", color = "grey60")) +
+  theme(axis.title = element_text(hjust = 1, size = 12)) +
+  theme(axis.text = element_text(hjust = 1, size = 10))
+  
+PP
 
-PP +
-  geom_segment(data = rdt5,
-               size=.5,
-               linetype=3,
-               aes(
-                 x = x,
-                 y = y,
-                 xend = xend,
-                 yend = yend
-               )) +
-  geom_label(
-    data = rdt5,
-    aes(
-      x = xend,
-      y = yend,
-      fill = difference,
-      label = str_wrap(name, 22)
-    ),
-    size = 3.5,
-    hjust = c(rep(0, 5), rep(1, 5))
-  )
+# other ways to label stuff
 
 # +
 #   geom_label_repel(
